5b32571e678578c38e783c2df345df0e7938b34b
U-DESKTOP-PJKB3JB\joshu <joshua.lucas@outlook.com>
2018-11-18 15:38:00 -0500

    adjusted paths for fxml

--- src/main/java/org/test/demogui/DemoGUI.java
+++ src/main/java/org/test/demogui/DemoGUI.java
@@ -19,7 +19,7 @@ public class DemoGUI extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/scenes/FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
         
